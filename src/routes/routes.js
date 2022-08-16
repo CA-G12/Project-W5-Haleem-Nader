e2f12141f5f1d4678eb019a15b34ef13e616b276
nader shakshak <nadershak149@gmail.com>
2022-08-16 22:01:07 +0300

    Handle all The pages releats #11

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,3 +2,9 @@ const { appendFile } = require('fs');
 const path = require('path');
 
 app.use(express.josn());
+
+
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.urlencoded({extended: true}));
+
+// app.use(express.static(path.join(__dirname, '..', 'public')));
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
  
+const path = require('path');
 
 const app = express();
 
@@ -8,3 +9,20 @@ const port = 4001;
 app.listen(port,()=>{
     console.log('listening on port '+port);
 })
+
+// routes
+
+app.use(express.static(path.join(__dirname,'..', 'public')));
+
+app.get('/404page', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','public','pages', '404page.html'));
+  });
+
+
+app.get('/500page', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','public','pages', '500page.html'));
+  });
+
+  app.get('/landing', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','public','pages', 'landing.html'));
+  });
