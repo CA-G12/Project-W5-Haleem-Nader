075d5a85a683343612ebfbf5d42d1445a6ea7e7e
nader shakshak <nadershak149@gmail.com>
2022-08-17 14:01:47 +0300

    upDate-logic.js

--- public/js/dom.js
+++ public/js/dom.js
@@ -5,8 +5,13 @@ const authorPic = document.getElementById("authorPic")
 const btnSearch = document.getElementById("searchBtn") 
 const quieryInput = document.getElementById("queryInput")
 
-btnSearch.addEventListener("click",)
-quieryInput.addEventListener("keypress",)
+btnSearch.addEventListener("click",(e) => {
+    e.preventDefault();
+    fetch(`/authors/${quieryInput.value}`)
+    .then(data => data.json())
+    .then(result => console.log(result));
+})
+// quieryInput.addEventListener("keypress",)
 
 const dom = {
     renderPage(quoteText,authorName,authorImgURL){
