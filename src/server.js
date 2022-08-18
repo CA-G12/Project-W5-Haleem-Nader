ddb5f7237d00d09905bf1a6690eec603c5d094d3
nader shakshak <nadershak149@gmail.com>
2022-08-18 09:33:15 +0300

    Fix all bugs we have relats #40

--- src/server.js
+++ src/server.js
@@ -13,13 +13,13 @@ const {serverError, clientErrors} = require('./routes')
 app.use(express.static(path.join(__dirname,'..', 'public')));
 
 app.get('/authors/:athName',(req,res) => {
-  fetch(`https://goquotes-api.herokuapp.com/api/v1/random?count=100`)
+  fetch(`https://goquotes-api.herokuapp.com/api/v1/random?count=2500`)
 .then(response =>{
   return  response.json()
 })
 .then(data => {
   const arr2 = data.quotes.filter((ele)=>{
-    return ele.author === req.params.athName
+    return ele.author === req.params.athName;
   });
   res.json(arr2)
 })
